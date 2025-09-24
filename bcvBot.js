eb50b72b367f52b83d5f0078faba71b5fc494b86
Christopher <chrisdelgadox@gmail.com>
2025-09-24 19:35:17 -0400

    Fix: ruta verificada y permisos para Chrome en Render

--- bcvBot.js
+++ bcvBot.js
@@ -7,7 +7,7 @@ async function actualizarValorBCV() {
     console.log('🟡 Iniciando scraping...');
 
 const browser = await puppeteer.launch({
-executablePath: '/usr/bin/chromium-browser',
+executablePath: '/opt/render/project/src/chrome-linux64/chrome',
   headless: true,
   args: ['--no-sandbox', '--disable-setuid-sandbox']
 });
